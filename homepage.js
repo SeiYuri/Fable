4859935dfb59184e3e81651abf05db6862a3e781
Brian Burch <Bburch05@gmail.com>
2018-07-03 20:13:52 -0400

    feat: Homepage-Logic rough complete

--- homepage.js
+++ homepage.js
@@ -2,29 +2,10 @@
 // <!-- Start button: ID #start-button -->
 // <!-- Player name text boxes: CLASS .player-name-input -->
 
-
+$(document).ready(function(){
 //Player Objects
 var playersIn = 0
-
-var player1 = {
-    name: "",
-    points : 0
-};
-
-var player2 = {
-    name: "",
-    points : 0
-};
-
-var player3 = {
-    name: "",
-    points : 0
-};
-
-var player4 = {
-    name: "",
-    points : 0
-};
+$(".player-list").append("<h6> Players: " + playersIn + "/ 4</h6>")
 
 var players = [];
 
@@ -41,20 +22,22 @@ $("#instructions-button").on("click", function(){
 
 $("#start-button").on("click", function(){
     // If all players are in then start the game.
-    if(playersIn = 4){
-        $("").css("display", "none")
+    if(playersIn === 4){
+        $(".title-screen").css("display", "none");
         //hide the title screen and show the main gameplay div
     }
 });
 
-$("#player-name-button").on("click", function(){
-    Event.preventDefault();
+$("#player-name-button").on("click", function(event){
+    event.preventDefault();
+
     //add the player name to the corresponding player object
     var nameInput = $(".player-name-input").val().trim();
+
+    $(".player-list").empty();
+
     if(nameInput){
-        console.log(nameInput);
         if (playersIn === 0){
-            console.log("Is it working?")
             var player1 = {
                 name: nameInput,
                 points : 0
@@ -64,6 +47,8 @@ $("#player-name-button").on("click", function(){
 
             //append player name to the users list
         }
+
+
         else if (playersIn === 1){
             var player2 = {
                 name: nameInput,
@@ -73,6 +58,8 @@ $("#player-name-button").on("click", function(){
             playersIn++;
             //append player name to the users list
         }
+
+
         else if (playersIn === 2){
             var player3 = {
                 name: nameInput,
@@ -82,6 +69,8 @@ $("#player-name-button").on("click", function(){
             playersIn++;
             //append player name to the users list
         }
+
+
         else if (playersIn === 3){
             var player4 = {
                 name: nameInput,
@@ -91,16 +80,20 @@ $("#player-name-button").on("click", function(){
             playersIn++;
             //append player name to the users list
             //append message stating that the room is full
-            console.log("Everybody is In! Start the Game")
+            console.log("Everybody is In! Start the Game");
         }
+
+
         else {
             //append message stating that the room is full
-            console.log("Game is full!")
-
+            console.log("Game is full!");
         }
     }
+    $(".player-list").append("<h6> Players: " + playersIn + "/ 4</h6>")
     for(var i= 0 ; i < players.length; i++) {
-        $(".player-list").append("<li><i class='fas fa-user'></i>" + players[i].name + "</li>");
+        $(".player-list").append("<li><i class='fas fa-user'></i>   " + players[i].name + "</li>");
     }
     
-})
+});
+
+});
