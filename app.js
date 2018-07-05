63690b43f5c9431641f42fc647219a4ada765045
Mark Jackson <35121685+markjackson02@users.noreply.github.com>
2018-07-05 11:46:25 -0400

    created function updateRandomContent

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-test
+$(document).ready(function() {
+
+    updateRandomContent();
+    function updateRandomContent() {
+        
+        var randomBoolean = Math.floor(Math.random()*2);
+        console.log(randomBoolean);
+        if (randomBoolean === 0) { /* update & display random image */
+            var randomImgQueryURL = "https://api.unsplash.com/photos/random?client_id=de1f3cde2de36e59880cc078d226a340adb625b535b3b059f97555a8e18fd26f";
+            var randomImgUrl = "";
+            $.ajax({
+                url: randomImgQueryURL,
+                method: "GET"
+            }).then(function(response) {
+                randomImgUrl = response.urls.regular;
+                var randomImg = $("<img>").attr({
+                    "src": randomImgUrl,
+                    "class": "random-img",
+                    "alt": "random image",
+                    "width": "300px"
+                });
+                $("#random-content").html(randomImg);
+            });
+        } else if (randomBoolean === 1) { /* update & display random word */
+            var randomWords = ["apple", "banana", "clementine", "dragonfruit"];
+            /* this is temporary until we have Words API working */
+            var randomArrayElement = Math.floor(Math.random()*4);
+            var randomWord = randomWords[randomArrayElement];
+            $("#random-content").html(randomWord);
+        }
+        
+    }
+    
+
+});
