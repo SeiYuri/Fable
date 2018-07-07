d5fbc5b63b0c4696c15b1d22caea0a13876d7c63
Mark Jackson <35121685+markjackson02@users.noreply.github.com>
2018-07-07 13:49:46 -0400

    added span with class

--- randomContent.js
+++ randomContent.js
@@ -1,4 +1,5 @@
 function updateRandomContent() {    
+    $("#random-content").empty();
     var randomBoolean = Math.floor(Math.random()*2);
     if (randomBoolean === 0) { /* update & display random image */
         var randomImgQueryURL = "https://api.unsplash.com/photos/random?client_id=de1f3cde2de36e59880cc078d226a340adb625b535b3b059f97555a8e18fd26f";
@@ -13,18 +14,18 @@ function updateRandomContent() {
                 "class": "random-img",
                 "alt": "random image"
             });
-            $("#random-content").html(randomImg);
+            $("#random-content").append(randomImg);
         });
     } else if (randomBoolean === 1) { /* update & display random word */
-        var randomWordQueryURL = "https://random-word-api.herokuapp.com/word?key=JHYBH4V4&number=1";
-        var randomWord = "";
+        var randomWordQueryURL = "https://random-word-api.herokuapp.com/word?key=U3UG725K&number=1";
         $.ajax({
             url: randomWordQueryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
-            randomWord = response;
-            $("#random-content").html(randomWord);
+            var randomWord = response;
+            var randomWordSpan = $("<span>").text(randomWord);
+            randomWordSpan.addClass("random-word");
+            $("#random-content").append(randomWordSpan);
         });
     }
 }
